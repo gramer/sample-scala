6df0b14499d75b7600837e50bbebd3b48ed74f52
gramer <gramer2188@gmail.com>
2014-07-07 15:55:07 +0900

    move folder

--- sample-scala/src/main/scala/com/gramer/address/Person.scala
+++
@@ -1,8 +0,0 @@
-package com.gramer.address
-
-class Person(n: String, a: Int, e: Option[String] = None) {
-    
-    var age: Int = a
-    var name: String = n
-    var email: String = e.getOrElse("")
-}
